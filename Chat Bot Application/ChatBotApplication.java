f56db8468f82cecbdaff62ef3f6815f86c8bd025
Kamalesh K <79313198+Kamalesh2006@users.noreply.github.com>
2023-01-22 16:52:47 +0530

    Update ChatBotApplication.java

--- Chat Bot Application/ChatBotApplication.java
+++ Chat Bot Application/ChatBotApplication.java
@@ -7,6 +7,7 @@ public class ChatBotApplication {
         Stack<ChatBotOptions> stackOfObject = new Stack<>();
         
         System.out.println("\n\n---- Welcome to chat-bot application ----\n\n");
+        int count =3;
 
         while(true){
 
@@ -21,26 +22,38 @@ public class ChatBotApplication {
 
             if(langIndex>2 || langIndex <0){
 
-                System.out.println("\nx-x-x-PLEASE ENTER THE CORRECT NUMBER-x-x-x\n");
+                
+                count--;
+                if(count==0){
+                    System.out.println("\n\tYou have reached the limit. Please Try Again after sometime.");
+                    break;
+                }
+                else{
+                    System.out.println("\nx-x-x-PLEASE ENTER THE CORRECT NUMBER-x-x-x\n");
+                }
                 continue;
 
             }
+            
 
             System.out.printf("\nYou have chosen %s language\n\n",lang[langIndex]);
+
+            ChatBotOptions menu  = new MainMenu();
+            
+            //to wait for the user to enter into main menu setting
+            try{
+                Thread.sleep(1500);
+                stackOfObject.push(menu);
+                menu.options(stackOfObject);
+            }
+            catch(InterruptedException ie){
+                System.out.println("Unexpected Error\n Restart again to continue");
+            }
             break;
+
         }
 
-        ChatBotOptions menu  = new MainMenu();
         
-        //to wait for the user to enter into main menu setting
-        try{
-            Thread.sleep(1500);
-            stackOfObject.push(menu);
-            menu.options(stackOfObject);
-        }
-        catch(InterruptedException ie){
-            System.out.println("Unexpected Error\n Restart again to continue");
-        }
 
     }
 }
