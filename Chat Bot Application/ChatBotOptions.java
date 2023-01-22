b3c58fa504e4a2ec566cfd6e4289ee0e5d98bd2a
Kamalesh K <79313198+Kamalesh2006@users.noreply.github.com>
2023-01-22 16:36:10 +0530

    Delete Chat Bot Application directory

--- Chat Bot Application/ChatBotOptions.java
+++
@@ -1,5 +0,0 @@
-import java.util.Stack;
-
-public interface ChatBotOptions {
-    public void options(Stack<ListDown> stack) throws InterruptedException;
-}
--- Chat Bot Application/ListDown.java
+++
@@ -1,6 +0,0 @@
-import java.util.Stack;
-public class ListDown implements ChatBotOptions {
-    public void options(Stack<ListDown> stack)throws InterruptedException{
-        System.out.println("Parent class ListDown method");
-    }
-}
