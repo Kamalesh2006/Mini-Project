a29563284ff15b8aa021d72de671e88e8c651786
Kamalesh K <79313198+Kamalesh2006@users.noreply.github.com>
2023-01-23 09:56:22 +0530

    Update Applicant.java

--- Interview Application/Applicant.java
+++ Interview Application/Applicant.java
@@ -1,5 +1,7 @@
 import java.util.regex.*;
 import java.util.LinkedList;
+
+
 public class Applicant {
 
         private String name;
@@ -17,20 +19,36 @@ public class Applicant {
         
     }
 
+    /**
+     * 
+     * @param email of String used to check the email is in correct format
+     * @return boolean (True if email is in correct format)
+     */
     private boolean validateEmail(String email){
+
         Pattern p = Pattern.compile("[a-zA-Z0-9][a-zA-Z0-9]*@[a-zA-Z0-9]+[.][com]");
         Matcher m = p.matcher(email);
+
         if(m.find()){
             return true;
         }else{
             System.out.println("\n---Please input the correct email id---");
             return false;
         }
+
     }
+
+    /**
+     * 
+     * @param num of long type, to check number is 10 digit and starts with 7|8|9
+     * @return boolean whether the format is correct or wrong
+     */
     private  boolean validatePhone(long num){
+
         Pattern p = Pattern.compile("[7-9][0-9]{9}");
         String number = Long.toString(num);
         Matcher m = p.matcher(number);
+
         if(number.length()==10 && m.find()){
             return true;
         }else{
@@ -40,7 +58,17 @@ public class Applicant {
         }
 
     }
+
+
     //factory method
+    /**
+     * 
+     * @param name applicant name of string type
+     * @param age applicant age of int type, it should be greater than 18 and lesser than 60
+     * @param email applicant email id of string type 
+     * @param number applicant phone number of int type and it should start either with 7 or 8 or 9
+     * @return Applicant object 
+     */
     public static Applicant addApplicant(String name, int age, String email, long number){
         
         Applicant applicant= new Applicant(name, age, email, number);
@@ -74,7 +102,9 @@ public class Applicant {
         }else{
             return null;
         }
+
         return applicant;
+
     }
     public String toString(){
         return "\t"+name+" "+age+" "+email+" "+number;
